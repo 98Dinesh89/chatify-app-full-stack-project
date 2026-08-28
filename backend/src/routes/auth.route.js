61eefbdc51f9cba1425027f4d5ca19ad2f0500cd
98Dinesh89 <dinsun9983@gmail.com>
2026-08-28 18:35:03 +0530

    rate limiting implimented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../contorllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 
